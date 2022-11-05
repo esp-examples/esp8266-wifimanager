65152ca2ef805d489780e7ccd8d18f79525d0df6
Daniel Niecke <daniel@niecke-it.de>
2022-11-05 20:26:41 +0100

    add comments and clean up code

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,6 @@
 #include "LittleFS.h"
 #include <ESP_DoubleResetDetector.h>
 
-String mqtt_server_val;
-String mqtt_port_val;
-String device_name_val;
-
-WiFiManagerParameter config_head("<h2>Additional Config</h2>");
-WiFiManagerParameter mqtt_server_head("<h3>MQTT Server</h3>");
-WiFiManagerParameter mqtt_port_head("<h3>MQTT Port</h3>");
-WiFiManagerParameter device_name_head("<h3>Device Name</h3>");
-
-const char *config_path = "/config.data";
-
-File file;
-
 // Number of seconds after reset during which a
 // subseqent reset will be considered a double reset.
 #define DRD_TIMEOUT 10
@@ -25,8 +12,22 @@ File file;
 // RTC Memory Address for the DoubleResetDetector to use
 #define DRD_ADDRESS 0
 
+// We use the DoubleResetDetector so that we can reset all
+// configurations that were made via the portal
 DoubleResetDetector drd(DRD_TIMEOUT, DRD_ADDRESS);
 
+// The config is stored in the root of our littlefs
+const char *config_path = "/config.data";
+
+String mqtt_server_val;
+String mqtt_port_val;
+String device_name_val;
+
+File file;
+
+/*
+  This function reads the 3 config parameters from the file specified by config_path in the correct order.
+*/
 void readConfig()
 {
   file = LittleFS.open(config_path, "r");
@@ -37,6 +38,9 @@ void readConfig()
   file.close();
 }
 
+/*
+  This function writes the 3 config parameters to the file specified by config_path.
+*/
 void writeConfig(const char *mqtt_server, const char *mqtt_port, const char *device_name)
 {
   file = LittleFS.open(config_path, "w");
@@ -49,6 +53,10 @@ void writeConfig(const char *mqtt_server, const char *mqtt_port, const char *dev
   file.close();
 }
 
+/*
+  This function writes the default config to the littlefs.
+  It does this by wrapping writeConfig().
+*/
 void writeDefaultConfig()
 {
   writeConfig("localhost", "1883", "test_device");
@@ -57,9 +65,9 @@ void writeDefaultConfig()
 void setup()
 {
   Serial.begin(9600);
-  // WiFiManager
-  // Local intialization. Once its business is done, there is no need to keep it around
+
   WiFiManager wifiManager;
+  // The wifiManager can print many messages when running in debug mode
   wifiManager.setDebugOutput(false);
 
   if (!LittleFS.begin())
@@ -71,12 +79,17 @@ void setup()
   file = LittleFS.open(config_path, "r");
   if (!file)
   {
+    // If no config is found the default config will be written to the fs.
+    // This ensures, that there is always a config in place.
     Serial.println("No config data found.");
     writeDefaultConfig();
   }
   file.close();
   if (drd.detectDoubleReset())
   {
+    // If a doubleReset was detexcted, we override our config with the
+    // default an reset the wifiManager. This will remove information such
+    // as SSID and password.
     Serial.println("Double reset -> resetting to default config!");
     writeDefaultConfig();
     wifiManager.resetSettings();
@@ -84,8 +97,14 @@ void setup()
 
   readConfig();
 
+  // We add our parameters and a few html tags to the wifiManager.
+  // This allows a user to enter these information in the portal.
+  WiFiManagerParameter config_head("<h2>Additional Config</h2>");
+  WiFiManagerParameter mqtt_server_head("<h3>MQTT Server</h3>");
   WiFiManagerParameter mqtt_server("server", "localhost", mqtt_server_val.c_str(), 40);
+  WiFiManagerParameter mqtt_port_head("<h3>MQTT Port</h3>");
   WiFiManagerParameter mqtt_port("port", "1883", mqtt_port_val.c_str(), 5);
+  WiFiManagerParameter device_name_head("<h3>Device Name</h3>");
   WiFiManagerParameter device_name("device_name", "", device_name_val.c_str(), 30);
 
   wifiManager.setCustomHeadElement("<style>body {background-color: lightgrey;}</style>");
@@ -101,7 +120,18 @@ void setup()
 
   writeConfig(mqtt_server.getValue(), mqtt_port.getValue(), device_name.getValue());
 
-  Serial.println("connected...yeey :)");
+  // Here we just print some information about the network
+  // the device is now connected to.
+  Serial.printf("The device is now connected to: %s\n", WiFi.SSID().c_str());
+  Serial.printf("Device IP: \t%s\n", WiFi.localIP().toString().c_str());
+  Serial.printf("Netmask: \t%s\n", WiFi.subnetMask().toString().c_str());
+  Serial.printf("Gateway: \t%s\n", WiFi.gatewayIP().toString().c_str());
+  Serial.printf("DNS IP: \t%s\n", WiFi.dnsIP().toString().c_str());
+
+  Serial.println("Additional Config");
+  Serial.printf("MQTT Server:\t%s\n", mqtt_server.getValue());
+  Serial.printf("MQTT Port:\t%s\n", mqtt_port.getValue());
+  Serial.printf("Device Name:\t%s\n", device_name.getValue());
 }
 
 void loop()
